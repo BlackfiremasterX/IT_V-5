ae55fd4ddd54c82004651cc4404a986840d6a02b
BlackfiremasterX <misharozental@gmail.com>
2021-01-26 18:47:36 +0700

    Решили пару задач111

--- src/main.kt
+++ src/main.kt
@@ -1,62 +1,50 @@
-////Добавить в калькулятор функцию расчета степени и извлечения из корня
-////
-//fun main()
-//{
-//    print("Введите первое число: ")
-//    var num1 = readLine().toString().toDouble()
-//    print("Введите второе число: ")
-//    var num2 = readLine().toString().toDouble()
-//    print("Выберите операцию: ")
-//    var oper = readLine()
-//    when(oper)
-//    {
-//        "+" -> println(num1+num2)
-//        "-" -> println(num1-num2)
-//        "/" -> println(num1/num2)
-//        "*" -> println(num1*num2)
-//        else -> print("Ошибка!")
-//    }
-//
-//
-//}
-//
+/*
+fun main() {
+    var truePassword = "123456"
+
+    println("Введите пароль")
+    var userPass = readLine()
+
+    if (truePassword == userPass) {
+        println("Добро пожаловать!")
+    }
+    else {
+        println("Пароль неверный")
+    }
+}*/
 
 
 
-//Begin7◦. Найти длину окружности L и площадь круга S заданного радиуса R:
-// L= 2·π·R,S=π·R2.
-// В качестве значенияπиспользовать 3.14
 
 //fun main()
 //{
-//    print("Введите радиус окружности: ")
-//    var R = readLine()!!.toInt()
-//    var π = 3.14
-//    var L = 2*π*R
-//    var S = π*R*R
-//    print("Длина окружности равна: $L\nПлощадь круга равна: $S")
+//    var Temperature_f = readLine()!!.toDouble()
+//    var Tc = (Temperature_f-32)*5/9
+//    print(Tc)
+//
 //}
+//    print("Введите скорость первого автомобиля: ")
+//    var V1 = readLine()!!.toDouble()
+//    print("Введите скорость второго автомобиля: ")
+//    var V2 = readLine()!!.toDouble()
+//    print("Введите расстояние на момент начала рассчета: ")
+//    var S_begin = readLine()!!.toDouble()
+//    print("Время прошедшее со старта: ")
+//    var T = readLine()!!.toDouble()
+//    var S_final =  S_begin+V1*T+V2*T
+//        print("Итоговый рассчет: $S_final")
 
-import kotlin.math.*
+
+//    var Week = readLine()!!.toInt()
+//    var Year = readLine()!!.toInt()
+import kotlin.random.Random
 fun main()
 {
-    print("Введите радиус большей окружности O1: ")
-    var R1 = readLine()!!.toInt()
-    print("Введите радиус меньшей окружности O2: ")
-    var R2 = readLine()!!.toInt()
-    var π = 3.14
-    var S1 = π*R1*R1
-    var S2 = π*R2*R2
-    var S_ring = S1 - S2
-    print("Площадь первого круга равна: $S1\nПлощадь второго круга равна: $S2\nПлощадь кольца равна: $S_ring")
-var q = R1.pow(2)
-
-    print(q)
+    var a = Random.nextInt(1,8)
 
 
 
 }
-
 
 
 
